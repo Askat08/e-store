7b9051a20533157eed7a031eb56a22de5b96b302
Askat B <Askat@MacBook-Pro-2.home>
2020-04-07 18:01:48 -0400

    gh pages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
+  <Router basename={window.location.pathname || ""}>
     <App />
   </Router>,
   document.getElementById("root")
