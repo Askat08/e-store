f51f8cea38ad52ca3f671e978e8263259ae2cf2e
Askat B <Askat@MacBook-Pro-2.home>
2020-04-07 17:47:05 -0400

    Updates

---
+++ precache-manifest.9a20d397d80f47dc1f429a27b078943b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305e8f08b003b2188edb5f73c96fa928",
+    "url": "/Askat08/e-store/index.html"
+  },
+  {
+    "revision": "e515886e24ec3a2d2dfa",
+    "url": "/Askat08/e-store/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "defd04f414e65a5817cc",
+    "url": "/Askat08/e-store/static/css/main.c8041810.chunk.css"
+  },
+  {
+    "revision": "e515886e24ec3a2d2dfa",
+    "url": "/Askat08/e-store/static/js/2.aeebead1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Askat08/e-store/static/js/2.aeebead1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defd04f414e65a5817cc",
+    "url": "/Askat08/e-store/static/js/main.85c838a9.chunk.js"
+  },
+  {
+    "revision": "40b39d518d33cff56b5e",
+    "url": "/Askat08/e-store/static/js/runtime-main.e0d177e8.js"
+  },
+  {
+    "revision": "5eea29e87b6bf5dc87146d894af294e2",
+    "url": "/Askat08/e-store/static/media/logo.5eea29e8.svg"
+  }
+]);
