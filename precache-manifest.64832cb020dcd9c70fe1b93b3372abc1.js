d070cd4dd92798f14f2a9c7c688a29f202c0c970
Askat B <Askat@MacBook-Pro-2.home>
2020-04-14 00:00:45 -0400

    Updates

---
+++ precache-manifest.64832cb020dcd9c70fe1b93b3372abc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68f474fee4fb73acfaf143fea97aaa1",
+    "url": "/e-store/index.html"
+  },
+  {
+    "revision": "270dc00e65aa7a96ab06",
+    "url": "/e-store/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d5d3ea03dd4a1d79eaa6",
+    "url": "/e-store/static/css/main.7f86b793.chunk.css"
+  },
+  {
+    "revision": "270dc00e65aa7a96ab06",
+    "url": "/e-store/static/js/2.ff9fc102.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-store/static/js/2.ff9fc102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d3ea03dd4a1d79eaa6",
+    "url": "/e-store/static/js/main.36c23de1.chunk.js"
+  },
+  {
+    "revision": "94d55ecbfc994d420f19",
+    "url": "/e-store/static/js/runtime-main.b0c67fb4.js"
+  },
+  {
+    "revision": "5eea29e87b6bf5dc87146d894af294e2",
+    "url": "/e-store/static/media/logo.5eea29e8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ff601179de62d07f2fad2943209a9ee.js"
+  "/e-store/precache-manifest.64832cb020dcd9c70fe1b93b3372abc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
